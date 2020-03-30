3dbcbd9b7638e01b1de0829a684cdc81a1cc4829
Yoga Wibowo Aji <yogawa92@gmail.com>
2020-03-30 13:38:42 +0700

    merapikan

--- swatch-gen.php
+++ swatch-gen.php
@@ -1,24 +1,24 @@
 <?php
 set_time_limit(0);
-echo $argc;
-if($argc!=4)
-{
-	echo "\r\n-=Swatch Configs Generator=-\r\nUsage: ".$argv[0]." listinput.txt outputfile servername\r\n";
-	die();
+
+if ($argc != 4) {
+	die("\r\n\033[0;36m-= Swatch Configs Generator =-\r\nUsage: " . $argv[0] . " listinput.txt outputfile servername\033[0;39m\r\n\n");
 }
-echo "\r\n-=Swatch Configs Generator=-\r\nUsage: $argv[0] listinput.txt outputfile servername\r\n";
-$servername=$argv[3];
-$output=fopen($argv[2],"w+");
-$sensitive=fopen($argv[1],"r");
-	while(! feof($sensitive))
-	{
-	$xpl = rtrim(fgets($sensitive));	
-	fwrite($output,"watchfor /$xpl/i\r\n");
-	fwrite($output,"pipe /etc/swatch/telebotnginx.sh \"Someone Accessed $xpl Via $servername\"\r\n");
-	fwrite($output,"throttle 00:00:10\r\n\r\n");
-	echo "$xpl Generated\r\n";
-	}
-echo "Finish";	
+
+echo "\r\n\033[0;36m-=Swatch Config Generator=-\033[0;39m\r\n\n";
+
+$servername = $argv[3];
+$output = fopen($argv[2], "w+");
+$sensitive = fopen($argv[1], "r");
+
+while (!feof($sensitive)) {
+	$xpl = rtrim(fgets($sensitive));
+	fwrite($output, "watchfor /$xpl/i\r\n");
+	fwrite($output, "pipe /etc/swatch/telebotnginx.sh \"Someone Accessed $xpl Via $servername\"\r\n");
+	fwrite($output, "throttle 00:00:10\r\n\r\n");
+	echo "\033[0;33m$xpl Generated\033[0;39m\r\n";
+}
+
+echo "\033[0;32m\nAll patterns successfully generated\033[0;39m";
 fclose($sensitive);
 fclose($output);
-?>
